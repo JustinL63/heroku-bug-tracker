be9ebf14c5b67fc77b7bb5290259e8d2c9d40f24
Justin Lamb <lambjg63@gmail.com>
2020-03-06 13:28:44 -0500

    login

--- heroku-bug/src/Containers/Login/Login.js
+++ heroku-bug/src/Containers/Login/Login.js
@@ -2,10 +2,57 @@ import React, { Component } from 'react'
 import classes from './Login.module.css'
 
 class Login extends Component {
+
+    state = {
+        user: {
+            email: '',
+            password: ''
+        },
+        isLoggedIn: false
+    }
+
+    onChangeHandler = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    onSubmitHandler = (event) => {
+        this.setState({
+           isLoggedIn: true
+        })
+        console.log(this.state.email + " " + this.state.password + ' ' + this.state.isLoggedIn )
+        event.preventDefault()
+    }
+
+    LoginHandler = () => {
+        this.setState({
+            isLoggedIn: true
+        })
+    }
+
+
+
     render() {
         return (
             <div className={classes.Login}>
-                <p>This will be the login</p>
+                <form>
+                    <label 
+                        placeholder='Email'
+                        >Email</label>
+                    <input
+                        onChange={this.onChangeHandler} 
+                        type='text' 
+                        name='email'/>
+                    <label 
+                         placeholder='Password' 
+                    >Password</label>
+                    <input
+                        onChange={this.onChangeHandler} 
+                        type='password' 
+                        name='password'/>
+                <button onClick={this.onSubmitHandler}>Submit</button>
+                </form>
             </div>
         )
     }
