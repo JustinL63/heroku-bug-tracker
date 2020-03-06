0c26795d2cdaf03d42f86c8e991828d04c9b30d9
Justin Lamb <lambjg63@gmail.com>
2020-03-06 12:02:20 -0500

    setup

---
+++ heroku-bug/src/Containers/Login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import classes from './Login.module.css'
+
+class Login extends Component {
+    render() {
+        return (
+            <div className={classes.Login}>
+                <p>This will be the login</p>
+            </div>
+        )
+    }
+}
+
+export default Login
