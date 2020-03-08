5150b55bd03aa4af96f75d50d4788ccf7063d069
Justin Lamb <lambjg63@gmail.com>
2020-03-08 08:30:25 -0400

    bootstrap

--- heroku-bug/src/Containers/Bugs/Bugs.js
+++ heroku-bug/src/Containers/Bugs/Bugs.js
@@ -1,17 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../Components/HOC/Auxillary'
-//npm   import Bug from './Bug'
+import axios from '../../axios-orders'
 
-const Bugs = (props) => {
+class Bugs extends Component {
 
-    return (
-        <Aux>
-            <h1>Bugs!!!</h1>
-            <h2>{props.otherstuff}</h2>
-            <p>{props.stuff}</p>
+    //this component pull the previously made bugs from the backend
 
-        </Aux>
-    )
+    state = {
+        bugList: null,
+        allBugs: null
+    }
+
+
+
+    
+    fetchBugsHandler = () => {
+        //this function gets the json info from firebase
+        axios.get('/bugs.json')
+        .then(response => {
+            console.log(response)
+            
+        })
+       
+       
+    }
+    setDataHandler = (info) => {
+        //this function takes the response and puts it into usable data
+        const newBugList = Object.keys(info)
+        .map(bugKey => {
+            return info[bugKey]
+        })
+        this.setState({
+            allBugs: newBugList
+        })
+        console.log(newBugList)
+
+
+    }
+
+    updatedBugsHandler = () => {
+        //this function will update the list of bugs
+
+    }
+    
+
+
+    render() {
+        return (
+            <Aux>
+                <h1>Bugs!!!</h1>
+        <p></p>
+                <button onClick={this.setDataHandler}>Set Data Handler</button>
+                <button onClick={this.fetchBugsHandler}>Get Bugs!!</button>
+            </Aux>
+        )
+    }
+
+   
 }
 
 export default Bugs
